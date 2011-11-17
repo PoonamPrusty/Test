e4be362b2a73accc773d1e26bb21a30673ea4478
Poonam Prusty <poonamprusty20@gmail.com>
2011-11-17 23:41:24 +0530

    message here

--- calculator.php
+++ calculator.php
@@ -1,23 +1,28 @@
 <?php
 	class Calc{
+		private $soln=0;
 		public function add($a,$b){
-			echo $a+$b;}
+			$this->soln=$a+$b;
+			echo "add: ".$this->soln;}
 		public function sub($a,$b){
-			echo $a-$b;}
+			$this->soln=$a-$b;
+			echo "diff: ".$this->soln;}
 		public function mul($a,$b){
-			echo $a*$b;}
+			$this->soln=$a+$b;
+			echo "prod: ".$this->soln;}
 		public function div($a,$b){
-			echo $a/$b;} 
+			$this->soln=$a+$b;
+			echo "div: ".$this->soln;}
 		}
-	$c=new Calc(3,4);
-	$c->add();
+	$c=new Calc;
+	$c->add(3,4);
 	class Caladv extends Calc{
 		public function _pow($x,$t){
-			echo pow($x,$t);}
+			echo "power: ".pow($x,$t);}
 		public function _sqrt($y){
-			echo sqrt($y);}
+			echo "sq.root: ".sqrt($y);}
 		public function _exp($z){
-			echo exp($z);}
+			echo "exponent: ".exp($z);}
 		}
 	$d=new Caladv;
 	$d->sub(4,2);
